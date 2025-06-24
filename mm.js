5aa3aab9babbe204bd3b8916816519bea2df1ea2
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-06-24 03:16:18 -0700

    Update mm.js

--- mm.js
+++ mm.js
@@ -1,6 +1,6 @@
 function displayImperial() {
   let mm = document.getElementById("mm").value || 0;
-  let conversion = mm/25.4
+  const conversion = mm/25.4
 
   let resultsHTML = "<h3>Result:</h3>";
   resultsHTML += "<p>"+ conversion.toFixed(2) + " inches</p>";
