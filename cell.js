781689381b9b78a4cc2ce6d9b85ff2fa5a700096
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-06-24 02:54:13 -0700

    Create cell.js

---
+++ cell.js
@@ -0,0 +1,8 @@
+function displayTemp() {
+  let cell = document.getElementById("cell").value || 0;
+  let conversion = (cell*1.8)+32
+
+  let resultsHTML = "<h3>Result:</h3>";
+  resultsHTML += `<p>${cell}°c = `+ conversion.toFixed(2) + "  °f</p>";
+  document.getElementById("results").innerHTML = resultsHTML;
+}
