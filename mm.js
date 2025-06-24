114627e138717dc3050198e16908d7041b34a245
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-06-24 04:49:32 -0700

    Delete mm.js

--- mm.js
+++
@@ -1,8 +0,0 @@
-function displayImperial() {
-  let mm = document.getElementById("mm").value || 0;
-  const conversion = mm/25.4
-
-  let resultsHTML = "<h3>Result:</h3>";
-  resultsHTML += "<p>"+ conversion.toFixed(2) + " inches</p>";
-  document.getElementById("results").innerHTML = resultsHTML;
-}
