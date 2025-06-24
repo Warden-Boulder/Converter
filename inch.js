2051f7495e488dcfed26ac5f604340275d99cec8
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-06-24 04:49:17 -0700

    Delete inch.js

--- inch.js
+++
@@ -1,8 +0,0 @@
-function displayMetric() {
-  let inches = document.getElementById("inch").value || 0;
-  const conversion = inches*25.4
-
-  let resultsHTML = "<h3>Result:</h3>";
-  resultsHTML += "<p>"+ conversion.toFixed(2) + " MM</p>";
-  document.getElementById("results").innerHTML = resultsHTML;
-}
