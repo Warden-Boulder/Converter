2a259d486c1f59e1aecf304f60bcea29bc801d74
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-06-24 04:48:52 -0700

    Delete cell.js

--- cell.js
+++
@@ -1,8 +0,0 @@
-function displayTemp() {
-  let cell = document.getElementById("cell").value || 0;
-  const conversion = (cell*1.8)+32
-
-  let resultsHTML = "<h3>Result:</h3>";
-  resultsHTML += `<p>${cell}°c = `+ conversion.toFixed(2) + "  °f</p>";
-  document.getElementById("results").innerHTML = resultsHTML;
-}
