6895cf7a15efe1cb489685566a240ef0050bff5e
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-06-24 04:49:05 -0700

    Delete fare.js

--- fare.js
+++
@@ -1,8 +0,0 @@
-function displayTemp() {
-  let fare = document.getElementById("fare").value || 0;
-  const conversion = (fare-32)*(5/9)
-
-  let resultsHTML = "<h3>Result:</h3>";
-  resultsHTML += `<p>${fare}°f = `+ conversion.toFixed(2) + "  °c</p>";
-  document.getElementById("results").innerHTML = resultsHTML;
-}
