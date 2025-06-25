5f92066e1eef5936a6789e08ccc3dd7d0a7bc03d
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-06-24 18:39:46 -0700

    Update length.js
    Changed the 2 original functions to reflect the conversions, added the new displayLength function

--- length.js
+++ length.js
@@ -1,17 +1,26 @@
-function displayMetric() {
-  let inches = document.getElementById("inch").value || 0;
+function IntoMM(){
+  let inches = document.getElementById("input").value || 0;
   const conversion = inches*25.4
 
   let resultsHTML = "<h3>Result:</h3>";
-  resultsHTML += "<p>"+ conversion.toFixed(2) + " MM</p>";
+  resultsHTML += `<p> ${inches} inch(es) = `+ conversion.toFixed(2) + " MM</p>";
   document.getElementById("results").innerHTML = resultsHTML;
 }
 
-function displayImperial() {
-  let mm = document.getElementById("mm").value || 0;
+function MMtoIn() {
+  let mm = document.getElementById("input").value || 0;
   const conversion = mm/25.4
   
   let resultsHTML = "<h3>Result:</h3>";
-  resultsHTML += "<p>"+ conversion.toFixed(2) + " inch(es)</p>";
+  resultsHTML += `<p>${mm} MM = ` + conversion.toFixed(2) + " inch(es)</p>";
   document.getElementById("results").innerHTML = resultsHTML;
+}
+
+function displayLength() {
+  let lengthSelect = document.getElementById("length").value;
+  if (lengthSelect === "in") {
+    IntoMM();
+  } else if (lengthSelect === "mm") {
+    MMtoIn();
+  }
 }
