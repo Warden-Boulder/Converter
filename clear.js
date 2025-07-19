a75996682e10f8a1e70c6d7facbaa29902bc9318
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-07-19 15:37:54 -0700

    Create clear.js

---
+++ clear.js
@@ -0,0 +1,8 @@
+function Clear() {
+  document.getElementById("results").innerHTML = "";
+  document.getElementById("LengthInput").value = "";
+  document.getElementById("MassInput").value = "";
+  document.getElementById("SpeedInput").value = "";
+  document.getElementById("tempInput").value = "";
+  document.getElementById("timeInput").value = "";
+}
