48bbddb9692af2fc7dffbd1f5b4e5d48e1030f17
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-07-27 23:35:45 -0700

    Update clear.js

--- clear.js
+++ clear.js
@@ -1,8 +1,13 @@
 function Clear() {
   document.getElementById("results").innerHTML = "";
+  document.getElementById("ElectricalInput").value = "";
+  document.getElementById("ElectricalInput2").value = "";
+  document.getElementById("EPInput").value = "";
+  document.getElementById("FuelInput").value = "";
   document.getElementById("LengthInput").value = "";
   document.getElementById("MassInput").value = "";
   document.getElementById("SpeedInput").value = "";
   document.getElementById("tempInput").value = "";
   document.getElementById("timeInput").value = "";
+  document.getElementById("VolumeInput").value = "";
 }
